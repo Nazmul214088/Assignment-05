d0f5e73644fc60eb6540a67deb756c1b150f370f
Nazmul Hoque <nazmul.info.duet@gmail.com>
2025-08-28 13:22:25 +0600

    complete responsive design

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        hind: ['Hind Madurai', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
